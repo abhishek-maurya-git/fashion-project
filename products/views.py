28de771542e42910cac2defb3befe6bdcbb288e9
Abhishek Maurya <abhishekmaurya9315@gmail.com>
2019-09-30 23:51:33 +0530

    details page is working

--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Products
 
 def index(request):
     products = Products.objects
     return render(request,'products/index.html', {'products':products})
+
+def details(request,product_id):
+    product = get_object_or_404(Products,pk=product_id)
+    return render(request, 'products/details.html', {'product':product})
